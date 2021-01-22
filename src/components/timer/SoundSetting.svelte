19a4278ba422006e1c635dfee630d3340146c0f0
Masahiro Sakuraba <rabasan1031@gmail.com>
2021-01-22 22:41:01 +0900

    soundのon/offをトグルにする

--- src/components/timer/SoundSetting.svelte
+++ src/components/timer/SoundSetting.svelte
@@ -1,14 +1,29 @@
-<label>
-    <input
-        type=checkbox
-        bind:checked={$isPlaySound}
-    >
-    Sound {#if $isPlaySound}ON{:else}OFF{/if}
-</label>
+<div>
+    <p on:click={onClick}>
+        Sound <i class="fas fa-2x" class:fa-toggle-on={$isPlaySound} class:fa-toggle-off={!$isPlaySound}></i>
+    </p>
+</div>
 
 <script lang="ts">
     import { isPlaySound } from '../../store';
+
+    const onClick = () => {
+        isPlaySound.set(!$isPlaySound);
+    };
 </script>
 
 <style>
+    div {
+        height: 32px;
+        width: 100px;
+    }
+
+    p {
+        line-height: 32px;
+        cursor: pointer;
+    }
+
+    i {
+        vertical-align: middle;
+    }
 </style>
