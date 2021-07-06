b65129a658a4b9e93930825b74f099da19d84aba
Masahiro Sakuraba <rabasan1031@gmail.com>
2021-07-07 01:20:46 +0900

    utilの処理を細分化

--- src/util.ts
+++ src/util.ts
@@ -7,19 +7,29 @@ export const padding = (num: number): string => {
     return num.toString().padStart(2, '0');
 };
 
+/**
+ * タイマーの合計秒数を求める
+ */
+export const calcTotalSeconds = (timer: Timer): number => {
+    return (timer.hour * 60 * 60) + (timer.minute * 60) + timer.second;
+};
+
+/**
+ * タイマーの合計秒数を求める
+ */
+export const totalSecondsToTimer = (totalSeconds: number): Timer => {
+    const hour = Math.floor(totalSeconds / (60 * 60));
+    const minute = Math.floor((totalSeconds - (hour * 60 * 60)) / 60);
+    const second = (totalSeconds - (hour * 60 * 60) - (minute * 60));
+    return { hour, minute, second };
+};
 
 /**
  * タイマーの設定情報リストから、合計時間を求める
  */
 export const calcTotalTime = (settings: TimerSetting[]): Timer => {
-    const totalSecond = settings.reduce((sum, setting) => {
-        const timer = setting.timer;
-        return sum + (timer.hour * 60 * 60) + (timer.minute * 60) + timer.second;
+    const totalSeconds = settings.reduce((sum, setting) => {
+        return sum + calcTotalSeconds(setting.timer);
     }, 0);
-
-    const hour = Math.floor(totalSecond / (60 * 60));
-    const minute = Math.floor((totalSecond - (hour * 60 * 60)) / 60);
-    const second = (totalSecond - (hour * 60 * 60) - (minute * 60));
-
-    return { hour, minute, second };
+    return totalSecondsToTimer(totalSeconds);
 };
